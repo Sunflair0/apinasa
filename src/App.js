2f21c4b79258dd8e04280de0546329020f6a5dcb
Rhonda Warner <sunflair_2000@yahoo.com>
2021-07-07 16:38:49 -0500

    appJS done

--- src/App.js
+++ src/App.js
@@ -15,6 +15,132 @@ import Form from "./components/Form";
 import TourInfo from "./components/TourInfo";
 import SatView from "./components/SatView";
 import WorldWeather from "./WorldWeather";
-import Asteroid from "./Asteroid";
+import AsterTrack from "./AsterTrack";
 
-export default App;
+function App({
+  client,
+  clearFavorites,
+  clearSearch,
+  clearClient,
+  ClearForm,
+  ClearAsterTrack,
+  clearWorldWeather,
+  clearSatView,
+}) {
+  return (
+    <Router>
+      <nav className="presenter">
+        {!client && (
+          <NavLink activeClassName="active" className="presenter" to="/login">
+            Login
+          </NavLink>
+        )}
+        {client && (
+          <>
+            <NavLink
+              activeClassName="active"
+              className="presenter"
+              to="/search"
+            >
+              Search
+            </NavLink>
+
+            <NavLink
+              activeClassName="active"
+              className="presenter"
+              to="/favorites"
+            >
+              Favorites
+            </NavLink>
+
+            <NavLink
+              activeClassName="active"
+              className="presenter"
+              to="/astertrack"
+            >
+              Asteroid Tracker
+            </NavLink>
+
+            <NavLink
+              activeClassName="active"
+              className="presenter"
+              to="/satview"
+            >
+              Satellite View
+            </NavLink>
+
+            <NavLink
+              activeClassName="active"
+              className="presenter"
+              to="/worldweather"
+            >
+              World Weather
+            </NavLink>
+
+            <NavLink activeClassName="active" className="presenter" to="/form">
+              Contact Us
+            </NavLink>
+
+            <NavLink
+              className="presenter"
+              to="/login"
+              onClick={() => {
+                clearFavorites();
+                clearSearch();
+                clearForm();
+                clearAsterTrack();
+                clearWorldWeather();
+                clearSatView();
+                clearClient();
+              }}
+            >
+              Logout
+            </NavLink>
+          </>
+        )}
+      </nav>
+      <main>
+        <Switch>
+          <ProtectedRoute path="/login" reqUser={false} component={Login} />
+          <ProtectedRoute path="/search" reqUser={true} component={Search} />
+          <ProtectedRoute
+            path="/favorites"
+            reqUser={true}
+            component={Favorites}
+          />
+          <ProtectedRoute
+            path="/astertrack"
+            reqUser={true}
+            component={AsterTrack}
+          />
+          <ProtectedRoute path="/satview" reqUser={true} component={SatView} />
+          <ProtectedRoute
+            path="/worldweather"
+            reqUser={true}
+            component={WorldWeather}
+          />
+          <ProtectedRoute path="/form" reqUser={false} component={Form} />
+          <Route path="*">
+            <Redirect to="/login" />
+          </Route>
+        </Switch>
+      </main>
+    </Router>
+  );
+}
+
+function mapStateToProps(state) {
+  return { client: state.user.client };
+}
+const mapDispatchToProps = {
+  clearFavorites,
+  clearClient,
+  clearSearch,
+  clearFavorites,
+  clearForm,
+  clearSatView,
+  clearWorldWeather,
+  ClearAsterTrack,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
