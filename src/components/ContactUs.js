0d20e7e1b9113e21e2b94fee248a3b2f007ccbd3
Rhonda Warner <sunflair_2000@yahoo.com>
2021-07-11 16:31:46 -0500

    Contact form complete

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,27 +1,39 @@
 import React, {useState} from "react";
 import { useForm } from "react-hook-form";
-export default function FormData(){ 
 
-const {register, handleSubmit, errors} =useForm
+
+const {register, handleSubmit, errors} =useForm();
 const onSubmit =(data) => {}
 
 
 function ContactUs() {
     return(
 
-<form onSubmit={handleSubmit}>
+<form onSubmit={handleSubmit(onSubmit)}>
 <div className="contact">
     <p className="titleform">SPACETOURS</p>
     <p className="formbody">What's on your mind?</p>
     <p>{client}</p>
     <input type="email" placeholder="Email" name="email" ref="register ({ required: true})}"></input>
-    <input type="text" placeholder="Subject" name="subject" ref="register({ required: true, minLength: 3; maxLength: 15;})}"></input>
-{errors.subject && <p> Subject must be descriptive</p>}
+    <select {...register ("subject")}>
+<option value="Newsletter">Newsletter</option>
+<option value="Insurance Claim">Insurance Claim</option>
+<option value="IntraPlanet Net">IntraPlanet Net</option>
+<option value="Refund">Refund</option>
+<option value="Submit Testimonial">Submit Testimonial</option>
+<option value="Rewards Program">Rewards Program</option>
+<option value="Other">Other</option>
 
-<input type="text" placeholder="Tell Us"></input>
+</select>
+
+<input type="text" placeholder="Tell Us" ref="register({ required: true, minLength: 3; maxLength: 15;})}"></input>
+{errors.subject && <p> Subject must be between 3 and 15 characters</p>}
+
     <input type="submit"></input>
 
 </div>
 </form>
 );
 }
+
+export default ContactUs
