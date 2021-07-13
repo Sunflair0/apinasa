93ae8a582f0d5f53c4326a23c24de5fd7470c1cb
Rhonda Warner <sunflair_2000@yahoo.com>
2021-07-13 17:37:11 -0500

    updated variables in Protected route

--- src/shared/ProtectedRoute.js
+++ src/shared/ProtectedRoute.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Rediret } from "react-router-dom";
 
-function ProtectedRoute({ clienttag, path, reqUser, component }) {
-  if ((clienttag && reqUser) || (!clienttag && !reqUser)) {
+function ProtectedRoute({ client, path, reqUser, component }) {
+  if ((client && reqUser) || (!client && !reqUser)) {
     return <Route path={path} component={component} />;
   } else {
-    return <Redirect to={reqUser ? "/login" : "/search"} />;
+    return <Rediret to={reqUser ? "/login" : "/search"} />;
   }
 }
-
 function mapStateToProps(state) {
   return {
     clienttag: state.client.clienttag,
