6ca6c2d89486c92f8fdada2d5c32ec83e6b6e59e
Rhonda Warner <sunflair_2000@yahoo.com>
2021-07-13 19:29:12 -0500

    forgot one!

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,14 +1,11 @@
 import { combineReducers } from "redux";
 import tourinfoReducer from "./tourinfoReducer"
-import clientReducer from "./clientReducer";
-import podReducer from "./podReducer";
 import formReducer from "./formReducer";
 import podReducer from "./podReducer";
 import searchesReducer from "./searchesReducer";
 import favoritesReducer from "./favoritesReducer";
 import contactusReducer from "./contactusReducer";
-
-
+import clientReducer from "./clientReducer";
 
 
 const rootReducer = combineReducers({
