c8c90fdf140f8eff112c5d875c619a506cae2d83
Rhonda Warner <sunflair_2000@yahoo.com>
2021-07-20 00:56:31 -0500

    Apod correcting page

--- src/components/Apod.js
+++ src/components/Apod.js
@@ -8,69 +8,72 @@ const Search =({
 startDate, 
 endDate, 
 byDate, 
-apod, 
-setApod,
+apods, 
+setApods,
 addFavorite, 
 deleteFavorite, 
 search, 
-setSearch 
+setSearch,
+description,
+copyright,
+favorites
+
 
 }) => {
 
-const [query, setQuery]= useState("")};
+const [query, setQuery]= useState("");
 const [apod, setApod]=useState("");
 const {data, loading, error} = useFetch(query);
-const apodId = useMemo(()=>{
+const apodIds = useMemo(()=>{
 return favorites.map((val)=> val.id);
 }, [favorites]);
 
-useEffect (()=> {
-if (data){
-setSearch(data.data)}})
+useEffect(() => {
+    if (data) {
+      setSearch(data.data);
+    }
+  }, [data, setSearch]);
 
- return (
-
+  return (
     <div>
       <div className="headtitle">Astronomy Picture of the Day
 </div>
+<form>
 <div className="menubox">
-          <label htmlFor="search">Gimme Picture of the Day!</label>
+          <label htmlFor="search"><h3>Gimme Picture of the Day</h3>!</label>
           <input
             id="search"
             value={apod}
-            onChange={(e) => setQuery(e.target.value)}
+            onChange={(e) => setApod(e.target.value)}
             placeholder="Picture of the Day"
           />
         </div>
-        <button
+<button
           className="btn"
           onClick={(e) => {
             e.preventDefault();
             setQuery(`&date=${byDate}`);
-          }}
-        >
-          Gimmie apod!
+}}> Gimmie a Pod!
         </button>
-<div>
+<div className="menubox">
     <h3> Gimmie a Range!</h3>
         <input type="date"
           className="btn"
           onClick={(e) => {
             e.preventDefault();
-            setStartDate(`&start_date==${startDate}`);
+            setQuery(`&start_date==${startDate}`);
           }}
         ></input> TO
             <input type="date"
           className="btn"
           onClick={(e) => {
             e.preventDefault();
-            setEndDate(`&end_date=${endDate}`);
+            setQuery(`&end_date=${endDate}`);
           }}
         >
         </input>
         </div>
-
-      
+      <div className="menubox">
       <div> <h3> Gimmie Five!</h3>
       <input type="button"
           className="btn"
@@ -78,9 +81,9 @@ setSearch(data.data)}})
             e.preventDefault();
             setQuery(`&count=5`);
           }}
-        ></input>
-      </div>
-    
+        >5</input>
+      </div></div>
+    </form>
       {loading && <div className="text-center">Loading apods</div>}
       {error && <div className="text-center">{error}</div>}
       {search && (
@@ -92,8 +95,8 @@ setSearch(data.data)}})
               isFav={apodIds.includes(apod.id)}
               id={apod.id}
               title={apod.title}
-              url={gif.images.original.url}
-description={description}
+              url={apod.images.original.url}
+          description={description}
       copyright ={copyright}        
 key={apod.id}
             />
@@ -102,20 +105,23 @@ key={apod.id}
       )}
     </div>
   );
+}
 
 function mapStateToProps(state) {
   return {
     clienttag: state.client.clienttag,
     favorites: state.favorites,
+search: state.search,
     apod: state.apod,
   };
 }
+
 const mapDispatchToProps = {
   deleteFavorite,
   addFavorite,
-  setapod,
-  byDate,
-  endDate,
-  startDate,
+  setSearch,
+ 
+
 };
+
 export default connect(mapStateToProps, mapDispatchToProps)(Search);
