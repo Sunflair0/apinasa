6b0b56c804c6e6556ce015ae1cd537b85f2e5424
Rhonda Warner <sunflair_2000@yahoo.com>
2021-07-19 21:44:40 -0500

    fixed serving errors, changed port

--- server/routes/ventures.routes.js
+++ server/routes/ventures.routes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const { byUserIDv, byVentIDv } = require("../models/ventures.model");
 
 
-router.get("/:user_id", (req, res) => {
+router.get("/user/:user_id", (req, res) => {
 return byUserIDv(res, req.params.user_id); 
 });
 
-router.get("/:vent_id", (req, res) => {
+router.get("id/:vent_id", (req, res) => {
 return byVentIDv(res, req.params.vent_id); 
 });
 
