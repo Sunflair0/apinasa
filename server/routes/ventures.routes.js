e29e2e776a8e2ba3584e339ca427fdc5c24827f1
Rhonda Warner <sunflair_2000@yahoo.com>
2021-07-21 16:55:14 -0500

    route testing

--- server/routes/ventures.routes.js
+++ server/routes/ventures.routes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const { byUserIDv, byVentIDv } = require("../models/ventures.model");
 
 
-router.get("/tour/:user_id", (req, res) => {
+router.get("/id/:user_id", (req, res) => {
 return byUserIDv(res, req.params.user_id); 
 });
 
-router.get("tour/:vent_id", (req, res) => {
+router.get("/id/:vent_id", (req, res) => {
 return byVentIDv(res, req.params.vent_id); 
 });
 
