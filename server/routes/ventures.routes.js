265a11cb80c537e586774f9995253bd210d8108e
Rhonda Warner <sunflair_2000@yahoo.com>
2021-07-23 16:56:34 -0500

    l

--- server/routes/ventures.routes.js
+++ server/routes/ventures.routes.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const { byUserIDv, byVentIDv } = require("../models/ventures.model");
 
 
+// ///// what vent/s do I have?
 router.get("/user/:user_id", (req, res) => {
 return byUserIDv(res, req.params.user_id); 
 });
 
-router.get("id/:vent_id", (req, res) => {
+// ///// info of Vent by ventID
+router.get("/vent/:vent_id", (req, res) => {
 return byVentIDv(res, req.params.vent_id); 
 });
 
