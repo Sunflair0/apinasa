6dc5aed1725451453b6b4b6f8bbe2e23e7c40b27
Rhonda Warner <sunflair_2000@yahoo.com>
2021-07-25 06:05:51 -0500

    creating second hook

--- src/components/VentureInfo2.js
+++ src/components/VentureInfo2.js
@@ -1,5 +1,28 @@
 import React from "react";
 
+LeftbarMenu.map((cube, vent_id)=> {
+return (
+<li key={vent_id} item={tours}>
+<Link to={vent_id.description}>
+{"img.vent_id"}
+<span>{cube.title}</span>
+</Link>
+</li>
+);
+})
+
+
+
+<div>
+let bigCube = vent_id.map(tour.length) => ({img:("link"), value: ventures.vent_id, text: ventures.description, price: ventures.cost} 
+
+<button className="ventBtn">Click to place order</button>)
+            </div>
+
+
+
+
+
  const VentureInfo2 =({item})=> {
     return (
         <div>
@@ -9,10 +32,5 @@ import React from "react";
             <p>{item.title}</p>
             <h4>{item.content}</h4></div>
             })}
-
-        <button className="ventBtn">Click to place order</button>
-            </div>
-    );
-}
 
 export default VentureInfo2;
