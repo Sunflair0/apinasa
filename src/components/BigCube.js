67d2406d6d362caf4fce87725fee3c0b5a9b2979
Rhonda Warner <sunflair_2000@yahoo.com>
2021-07-26 15:46:27 -0500

    change menu

---
+++ src/components/BigCube.js
@@ -0,0 +1,13 @@
+// import React from "react";
+
+ function BigCube(){ 
+
+    return (
+        <div>
+        
+        <button className="ventBtn">Click to place order</button>
+            </div>
+    );
+}
+
+ export default BigCube;
