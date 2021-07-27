73f4c58c8a67ed44ef476539c0f67783aeca58b8
Rhonda Warner <sunflair_2000@yahoo.com>
2021-07-27 09:02:30 -0500

    BigCube addition

---
+++ src/components/VentConsole2.js
@@ -0,0 +1,34 @@
+import React, {useState} from "react";
+import { Link } from "react-router-dom";
+import BigCube from "./BigCube";
+
+
+export default function MenuPop() {
+  const [isPop, setIsPop] = useState(false);
+return (
+<>
+<ul className="nav-menu-items">
+return( 
+MenuPop.map((item, index) => {
+
+<li className="wrapper"><button onClick={() => setIsPop(true)}>
+<div key={index}>
+<Link to={item.path}>
+ {item.img}
+<span>{item.tour}</span> 
+
+<BigCube open={isPop} onClose ={()=> setIsPop(false)}></BigCube>
+<div className="more">{BigCube()}</div>
+
+</Link>
+</div>
+</button>
+</li>
+}))
+</ul>
+
+</>)}
+
+
+export default MenuPop;
+
