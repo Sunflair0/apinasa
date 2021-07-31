c08f921ba11f342e0fc0665cb774250a96dc70ab
Rhonda Warner <sunflair_2000@yahoo.com>
2021-07-31 01:48:07 -0500

    Planets done and named. Login form needs centering

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,84 +7,90 @@ import { Link } from "react-router-dom";
 const Login = ({ setClient }) => {
   const [clienttag, setClienttag] = useState("");
   const [password, setPassword] = useState("");
-const {callAPI: loginCall} = useFetch("POST");
-const [error, setError]=useState(null)
+  const { callAPI: loginCall } = useFetch("POST");
+  const [error, setError] = useState(null)
 
 
   return (
     <>
+      <div className="shell">
+        <div className="circle15" title="Sun/Sol"></div>
+        <div className="therest">
+          <div className="logintop">Welcome to SpaceTours</div>
 
-      <div className="logintop">Welcome to SpaceTours</div>
+          <form className="loginform">
+<div className="Form">
+            <label htmlFor="clienttag" ></label>
+            <div id="space"> <input placeholder="Name"
+              id="clienttag"
+              onChange={(e) => setClienttag(e.target.value)}
+              value={clienttag}
+            /></div>
+            <label htmlFor="password" ></label>
+            <div id="space"><input placeholder="Password"
+              type="password"
+              id="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            /></div>
+            <button
+              className="btn"
+              onClick={async (e) => {
+                e.preventDefault();
+                if (
+                  clienttag.length > 4 &&
+                  password.length > 4 &&
+                  clienttag.length <= 20 &&
+                  password.length <= 20
+                ) {
+                  setError(null);
+                  let res = await loginCall("/api/clients/login", {
+                    clienttag,
+                    password,
+                  });
+                  if (res.error) {
+                    return setError(res.error);
+                  }
+                  console.log(res);
+                }
+              }}>
+              ENTER
+            </button>
+            <Link className="sign" to="/signup"><button type="button">Sign Up!</button></Link>
+          </div></form>
 
-      <form className="loginform">
-        
-          <label htmlFor="clienttag" ></label>
-          <div id="space"> <input placeholder="Name"
-            id="clienttag"
-            onChange={(e) => setClienttag(e.target.value)}
-            value={clienttag}
-          /></div>
-        
-        
-          <label htmlFor="password" ></label>
-          <div id="space"><input placeholder="Password"
-            type="password"
-            id="password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-          /></div>
-                <button
-          className="btn"
-          onClick={async (e) => {
-e.preventDefault();
-           if (
-              clienttag.length > 4 &&
-              password.length > 4 &&
-              clienttag.length <= 20 &&
-              password.length <= 20
-            ) {
-              setError(null);
-              let res = await loginCall("/api/clients/login", {
-                clienttag,
-                password,
-              });
-              if (res.error) {
-                return setError(res.error);
-              }
-console.log(res);
-            
-            }
-          }}
+          <div>{error}</div>
+        </div> </div>
+       <div className="circle5" title="Mercury"></div>
 
-        >
-          Login
-        </button>
+<div className="earthmoon">
+          <div className="circle" title="Earth"></div>
+        <div className="circle7" title="Moon/Luna"></div></div>
+ 
+          <div className="circle1" title="Pluto"></div>
+          <div className="circle2" title="Ganymede"></div>
+          <div className="circle3" title="Mars"></div>
+          <div className="circle4" title="Callisto"></div>
+         
+          <div className="circle6" title="Mimas"></div>
+          <div className="circle8" title="Titan"></div>
 
-<Link className="btn" to="/signup">Create Account</Link>
-      </form>
-<div className="circle"></div>
-<div className="circle1"></div>
-<div className="circle2"></div>
-<div className="circle3"></div>
-<div className="circle4"></div>
-<div className="circle5"></div>
-<div className="circle6"></div>
-<div className="circle7"></div>
-<div className="circle8"></div>
-<div className="circle9"></div>
-<div className="circle10"></div>
-<div className="circle11"></div>
-<div className="circle12"></div>
-<div className="circle13"></div>
-<div className="circle14"></div>
-<div className="circle15"></div>
-<div>{error}</div>
-    </>
+
+          <div className="circle9" title="Uranus"></div>
+          <div className="circle10" title="Ariel"></div>
+          <div className="circle11" title="Titania"></div>
+          <div className="circle12" title="Umbriel"></div>
+          <div className="circle13" title="Oberon"></div>
+          <div className="circle14" title="comet"></div>
+
+
+
+ </>
   );
 };
 
 const mapStateToProps = (state) => {
   return {};
 };
-const mapDispatchToProps = { setClient,};
+const mapDispatchToProps = { setClient, };
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
