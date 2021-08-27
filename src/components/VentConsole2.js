e69129a24a1ac3514717a655763215b852154c79
Rhonda Warner <sunflair_2000@yahoo.com>
2021-08-27 01:11:56 -0500

    .

--- src/components/VentConsole2.js
+++ src/components/VentConsole2.js
@@ -22,7 +22,6 @@ export default function VentConsole2() {
 // MenuPop.map((item, index) => {
 
 // <li className="wrapper"><button onClick={() => setIsPop(true)}>
-<<<<<<< HEAD
 // <div key={index}>
 // <Link to={item.path}>
 //  {item.img}
@@ -39,23 +38,5 @@ export default function VentConsole2() {
 // </ul>
 
 // </>)}
-=======
-{/* <div key={index}>
-<Link to={item.path}>
- {item.img}
-<span>{item.tour}</span>  */}
-
-{/* <BigCube open={isPop} onClose ={()=> setIsPop(false)}></BigCube>
-<div className="more">{BigCube()}</div>
-
-</Link>
-</div>
-</button>
-</li>
-}))
-</ul>
-
-</>)} */}
->>>>>>> master
 
 
