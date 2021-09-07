d211142ca199ebe323d326b0f57f7eb0b1df0bb7
Sunflair0 <62845098+Sunflair0@users.noreply.github.com>
2021-09-07 01:25:41 -0500

    Restored Carousel

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import useFetch from "../hooks/useFetch";
 import * as AiIcons from "react-icons/ai";
-import TourGuide from "./TourGuide";
-import signUpMess from "./TourGuide"
+import { Carousel } from "react-responsive-carousel";
 
 const Signup = () => {
   const [clienttag, setclienttag] = useState("");
@@ -36,7 +35,22 @@ const Signup = () => {
           />
         </div>
 <div className="outerS"> <div className="signMess">
-<signUpMess />
+<div className="carousel-wrapper">
+					<Carousel autoPlay useKeyboardArrows={true} infiniteLoop={true} interval={6500} showArrows={true} showStatus={false} showIndicators={false} showThumbs={false} >
+						<div style={{ color: "#15f4ee", marginTop: "50px" }}>
+							This is your TourGuide. Please keep it with you at all times while on the site to access our amazing deals.
+						</div>
+						<div style={{ color: "#15f4ee", marginTop: "50px" }}>
+							This model lights up. It is solar charged but can and will power down with extensive use. Touch the gray action stripe at the top to activate the light.
+						</div>
+						<div style={{ color: "#15f4ee", marginTop: "50px" }}>
+							Flip the TourGuide to select which Venture you want next.
+						</div>								
+						<div style={{ color: "#15f4ee", marginTop: "50px" }}>
+							Now that you have an account, hit the GO button for your next adventure. Keep this device with you to access Ventures on reverse side.
+						</div>
+					</Carousel>
+				</div>			
 </div>
         <button
           className="btnGroup" 
