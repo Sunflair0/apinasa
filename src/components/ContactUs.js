3463e6d5b274caa32e3f8475f2636e202f34b2c9
Sunflair0 <62845098+Sunflair0@users.noreply.github.com>
2021-09-17 22:19:30 -0500

    Contact us form started

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -11,14 +11,14 @@ const ContactUs = ({
 
   return (
     <>
-      <div className="flex3">
+      <div className="">
         <div className="glassForm">
           <div>Contact Us
             <form onSubmit={handleSubmit}>
 
-              <textarea type="text" required value={text} onChange={(e) => setText(e.target.value)} />
-              <input type="email" placeholder="Email" name="email" ></input>
-              <select name="selectList" id="selectList">
+<input type="submit" ></input>
+              <input className="textEmail" type="email" placeholder="Email" name="email" ></input>
+              <select className="textDrop" name="selectList" id="selectList">
                 <option value="Newsletter" label="Newsletter">Newsletter</option>
                 <option value="Insurance Claim" label="Insurance Claim">Insurance Claim</option>
                 <option value="IntraPlanet Net" label="IntraPlanet Net">IntraPlanet Net</option>
@@ -31,7 +31,10 @@ const ContactUs = ({
               {/* <input type="text" placeholder="Tell Us" ref="register({ required: true, minLength: 3; maxLength: 15;})"></input>
  <p> Subject must be between 3 and 15 characters</p>  */}
 
-              <input type="submit"></input>
+              
+              <textarea className="textForm" type="text" required value={text} placeholder="TELL US" onChange={(e) => setText(e.target.value)}
+                rows={17}
+                cols={50} />
             </form>
           </div>
         </div>
