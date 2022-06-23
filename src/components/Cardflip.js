e7acbd503fde60283a84f130ed5425b933557759
Sunflair0 <62845098+Sunflair0@users.noreply.github.com>
2022-06-23 11:14:29 -0500

    .

--- src/components/Cardflip.js
+++ src/components/Cardflip.js
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, {} from "react";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import ReactCardFlip from 'react-card-flip';
 
-export const Cardflip = () => {
-  return (
-    <div>Cardflip</div>
-  )
-}
+
+class Cardflip extends React.Component {
+    constructor() {
+      super();
+        this.state = {
+        isFlipped: false
+      };
+      this.handleClick = this.handleClick.bind(this);
+      
+    }
+  
+    handleClick(e) {
+      e.preventDefault();
+      this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
+    }
+  
+    render() {
+      return (
+        <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical">
+          <Login>
+    
+            <button onClick={this.handleClick}>Click to flip</button>
+          </Login>
+  
+          <SignUp>
+            
+            <button onClick={this.handleClick}>Sign</button>
+          </SignUp>
+        </ReactCardFlip>
+      )
+    }
+  }
+
+  export default (Cardflip);
