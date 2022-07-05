c8de07c067ecd16ffc3b7fa765b74d5b987b6708
Sunflair0 <62845098+Sunflair0@users.noreply.github.com>
2022-07-04 22:17:48 -0500

    login gets in again

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,12 @@
 import React, { useState } from "react";
-import useFetch from "../hooks/useFetch";
 import { setClient } from "../redux/actions";
 import { connect } from "react-redux";
+
 import TourGuideLight from "./TourGuideLight";
 
 const Login = ({ setClient }) => {
   const [clienttag, setClienttag] = useState("");
   const [password, setPassword] = useState("");
-  const {callAPI: loginCall } = useFetch("POST");
-  const [error, setError] = useState(false);
-
 
   return (
     <>
@@ -23,7 +20,7 @@ const Login = ({ setClient }) => {
               id="clienttag"
               onChange={(e) => setClienttag(e.target.value)}
               value={clienttag} />
-            <label htmlFor="password"></label>
+            <label htmlFor="password" ></label>
             <input placeholder="Password"
               type="password"
               id="password"
@@ -32,10 +29,9 @@ const Login = ({ setClient }) => {
             /></div>
           <div className="outerS">
             <div className="gmessage">Now that you have an account, hit the GO button for your next adventure. Keep this device with you to access Ventures on reverse side.</div>
-            <button
-              style={{ transform: "translateX(6px)" }}
+            <button style={{ transform: "translateX(6px)" }}
               className="btnGroup"
-              onClick={async (e) => {
+              onClick={(e) => {
                 e.preventDefault();
                 if (
                   clienttag.length > 4 &&
@@ -43,28 +39,18 @@ const Login = ({ setClient }) => {
                   clienttag.length <= 20 &&
                   password.length <= 20
                 ) {
-                  setError("");
-                  let res = await loginCall("/api/clients/login", {
-                    clienttag,
-                    password,
-                  });
-                  if (res.error) {
-                    return setError(res.error)
-                  } else {
-                    setClient(clienttag);
-                  }
+                  setClient(clienttag);
                 }
-              }
-              }>
+              }}>
               GO
             </button>
           </div>
           <div className="outerS">
-            <div className="smessage">If  you have not had the opportunity to create an account, please sign up before entering the site.</div>
+            <div className="smessage">
+              If  you have not had the opportunity to create an account, please sign up before entering the site.</div>
           </div>
         </div>
       </form>
-      <div >{error}</div>
     </>
   );
 };
