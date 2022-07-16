b53242ffc1340d52159f44b7cd3c3b42fa19d67e
Sunflair0 <62845098+Sunflair0@users.noreply.github.com>
2022-07-15 21:44:08 -0500

    icons on navbar object-object

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {
 } from "./redux/actions";
 import {
   BrowserRouter as Router, Routes, Route, Navigate, NavLink, Link,
-  nav, footer
+  nav
 } from "react-router-dom";
 import { ToggleProvider } from "./ToggleContext";
 import "./App.css";
@@ -42,133 +42,56 @@ import TourAddOns from "./components/TourAddOns";
 import VentureTours from "./pages/VentureTours";
 
 
-function App({ clienttag, clearBigCube, clearForm, clearApod, clearAlbum, clearClient, clearContactUs, clearBuyVent }
+function App({ clienttag }
 ) {
-
   return (
+    <>
+      <div style={{ backgroundImage: "url(../assets/stars.png)", zIndex: "-100" }}>
+        <div style={{ backgroundImage: "url(../assets/twink.png)", zIndex: "-90", animation: "twink 700s linear infinite" }}>
 
-    <div style={{ backgroundImage: "url(../assets/stars.png)", zIndex: "-100" }}>
-      <div style={{ backgroundImage: "url(../assets/twink.png)", zIndex: "-90", animation: "twink 700s linear infinite" }}>
-
-        <ToggleProvider>
-          <div className="flex_backing">
-            <Router >
-              <Routes>
-
-                {/* {!clienttag && ()} */}
-                <Route path="loginpage" element={<LoginPage />} />
-
-
-                <Route path="/" element={<PagesTemplate />} >
-                  <Route index element={<Home />} />
-                  <Route path="about" element={<About />} />
-                  <Route path="nasa" element={<NASA />}>
-                    <Route path="apod" element={<APOD />}>
-                      <Route path="apodtoday" element={<ApodToday />} />
-                      <Route path="apodchoose" element={<ApodChoose />} />
-                      <Route path="apodrange" element={<ApodRange />} />
-                      <Route path="apodgimme5" element={<ApodGimme5 />} />
+          <ToggleProvider>
+            <div className="flex_backing">
+              <Router >
+                <Routes>
+                  <Route path="loginpage" element={<ProtectedRoute isPrivate={false}> <LoginPage />
+                  </ProtectedRoute>
+                  }></Route>                  
+                    <Route path="/" element={<ProtectedRoute isPrivate={false}><PagesTemplate /></ProtectedRoute>} >
+                    <Route index element={<Home />} />
+                    <Route path="about" element={<About />} />
+                    <Route path="nasa" element={<NASA />}>
+                      <Route path="apod" element={<APOD />}>
+                        <Route path="apodtoday" element={<ApodToday />} />
+                        <Route path="apodchoose" element={<ApodChoose />} />
+                        <Route path="apodrange" element={<ApodRange />} />
+                        <Route path="apodgimme5" element={<ApodGimme5 />} />
+                      </Route>
+                      <Route path="mer" element={<MER />}>
+                      </Route>
+                      <Route path="earth" element={<EARTH />}>
+                      </Route>
                     </Route>
-                    <Route path="mer" element={<MER />}>
+                    <Route path="mypage" element={<MyPage />}>
+                      <Route path="album" element={<Album />} />
+                      <Route path="mypage" element={<MyPage />} />
                     </Route>
-                    <Route path="earth" element={<EARTH />}>
+                    <Route path="venturetours" element={<VentureTours />} />
+                    <Route path=":tourId" element={<TourInfo />} />
+                    <Route path=":addOnsID" element={<TourAddOns />} />
+                    <Route path="testimonials" element={<Testimonials />} />
+                    <Route path="contactus" element={<ContactUs />}>
+                      <Route path="form" element={<Form />} />
                     </Route>
+                    <Route path="*" element={<Error />} />
                   </Route>
-                  <Route path="mypage" element={<MyPage />}>
-                    <Route path="album" element={<Album />} />
-                    <Route path="mypage" element={<MyPage />} />
-                  </Route>
-                  <Route path="venturetours" element={<VentureTours />} />
-                  <Route path="venturetours/:tourId" element={<TourInfo />} />
-                  <Route path="venturetours/:addOnsID" element={<TourAddOns />} />
 
-                  <Route path="contactus" element={<ContactUs />}>
-                    <Route path="form" element={<Form />} />
-                  </Route>
-                  <Route path="*" element={<Error />} />
-                </Route>
-             
-              {clienttag && (
-                <>
-
-                  <ul>            
-
-                    <li> <Route
-                      className="menu1items"
-                      to="/LoginPage"
-                      onClick={() => {
-                        clearApod();
-                        clearBigCube();
-                        clearBuyVent();
-                        clearClient()
-                        clearContactUs();
-                        clearAlbum();
-                        clearForm();
-                      }}
-                    >
-                      Logout
-                    </Route></li>
-                  </ul>
-                </>
-              )}</Routes>
-
-            <Routes>
-              <>
-                {/* <ProtectedRoute path="/album" reqUser={true} component={Album} />
-
-                <ProtectedRoute path="/bigcube" reqUser={true} component={BigCube} />
-
-                <ProtectedRoute path="/buyvent" reqUser={true} component={BuyVent} />
-
-                <ProtectedRoute path="/apod" reqUser={true} component={APOD} />
-
-                <ProtectedRoute path="/apodtoday" reqUser={true} component={ApodToday} />
-
-                <ProtectedRoute path="/apodchoose" reqUser={true} component={ApodChoose} />
-
-                <ProtectedRoute path="/apodgimme5" reqUser={true} component={ApodGimme5} />
-
-                <ProtectedRoute path="/apodrange" reqUser={true} component={ApodRange} />
-
-                <ProtectedRoute path="/loginpage" reqUser={false} component={LoginPage} />
-
-                <ProtectedRoute path="/contactus" reqUser={true} component={ContactUs} />
-
-                <ProtectedRoute path="/earth" reqUser={true} component={EARTH} />
-
-                <ProtectedRoute path="/form" reqUser={true} component={Form} />
-
-                <ProtectedRoute path="/ipn" reqUser={true} component={Ipn} />
-
-                <ProtectedRoute path="/login" reqUser={false} component={Login} />
-
-                <ProtectedRoute path="/mer" reqUser={true} component={MER} />
-
-                <ProtectedRoute path=="home" reqUser={false} component={Signup} />
-
-                <ProtectedRoute path="/home" reqUser={true} component={Home} />
-
-                <ProtectedRoute path="/tourguidelight" reqUser={true} component={TourGuideLight} />
-
-                <ProtectedRoute path="/tourguidemessages" reqUser={true} component={TourGuideMessages} />
-
-                <ProtectedRoute path="/tourinfo" reqUser={true} component={TourInfo} />
-
-                <ProtectedRoute path="/TourAddOns" reqUser={true} component={TourAddOns} />
-
-                <ProtectedRoute path="/ventconsole" reqUser={true} component={VentConsole} />
-
-                <ProtectedRoute path="/venturetour" reqUser={true} component={VentureTours} /> */}
-
-                {/* <Route path="*" element={<Error />} /> */}
-              </>
-            </Routes>
-
-          </Router>
-      </div>
-    </ToggleProvider>
+                </Routes>
+              </Router>
+            </div>
+          </ToggleProvider>
+        </div >
       </div >
-    </div >
+    </>
   );
 };
 
