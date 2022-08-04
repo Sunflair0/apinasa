2a7bae5fdb9163d9398d1b5ffac6c947f003c19b
Sunflair0 <62845098+Sunflair0@users.noreply.github.com>
2022-08-04 15:08:31 -0500

    .

---
+++ src/components/Junk.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Junk = () => {
+	return (
+		<>
+			<div className="content_flexbox">
+				<div className="circle3"></div>
+				<div className="boxFade">
+					<div className="fadeBox">
+						<div className="iris"></div>
+						<div className="bubble"></div>
+						<div className="marsa"></div>
+					</div>
+				</div>
+			</div>
+		</>
+	)
+}
+
+
+export default Junk
