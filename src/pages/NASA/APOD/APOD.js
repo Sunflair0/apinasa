6032e5deff1d3b01c7d635d24ae54701e1aca1f5
Sunflair0 <62845098+Sunflair0@users.noreply.github.com>
2022-08-10 14:31:18 -0500

    .

--- src/pages/NASA/APOD/APOD.js
+++ src/pages/NASA/APOD/APOD.js
@@ -1,67 +1,51 @@
 import React, { useState, } from 'react';
-import { Link, Outlet } from 'react-router-dom';
-import ApodToday from './ApodToday';
-import ApodChoose from './ApodChoose';
-import ApodGimme5 from './ApodGimme5';
-import ApodRange from './ApodRange';
+import { NavLink, Outlet } from 'react-router-dom';
 
 export default function Apod() {
-  const [showToday, setShowToday] = useState(true);
-  const [showChoose, setShowChoose] = useState();
-  const [showGimme5, setShowGimme5] = useState();
-  const [showRange, setShowRange] = useState();
   const [selection, setSelection] = useState("")
-  const options = ["today", "choose", "gimme", "range"];
+  // const options = ["today", "choose", "gimme", "range"];
 
   return (
     <>
-      <div className="content_flexbox">
-        <div>
-<div className='flex3'>
-          <div className=" ">{options.map(opt => <button className="clear" type="button" key={opt} onClick={e => setSelection(opt)}>{opt}</button>)}
-          </div></div>
-          <div className=" apodBox apodSee">
-            <Link className='apodButton'
-              to="apodtoday"
+      <div className='apodPage'>        
+        <div className="apodButtons">
+          <NavLink to="apodtoday">
+            <div className='apodButton today'
               style={{
                 backgroundImage: "url(../../assets/today.png)"
               }}
-              onClick={() => setShowToday(showToday)} />
-          
+              onClick={() => setSelection(selection)} />
+          </NavLink>
 
-            <Link className='apodButton'
-              to="apodchoose"
+          <NavLink to="apodchoose">
+            <div className='apodButton choose'
               style={{
                 backgroundImage: "url(../../assets/choose.png)"
               }}
-              onClick={() => setShowChoose(showChoose)} />
-           
+              onClick={() => setSelection(selection)} />
+          </NavLink>
 
-            <Link className='apodButton'
-              to="apodgimme5"
+          <NavLink to="apodgimme5">
+            <div className='apodButton gimme'
               style={{
                 backgroundImage: "url(../../assets/gimme.png)"
               }}
-              onClick={() => setShowGimme5(showGimme5)} />          
+              onClick={() => setSelection(selection)} />
+          </NavLink>
 
-            <Link className='apodButton'
-              to="apodrange"
+          <NavLink to="apodrange">
+            <div className='apodButton range'
               style={{
                 backgroundImage: "url(../../assets/range.png)"
               }}
-              onClick={() => setShowRange(showRange)} />           
-          </div >
-        </div>
+              onClick={() => setSelection(selection)} />
+          </NavLink>
 
-        {/* <div className="">
-          <p>{selection === "today" && <ApodToday />}</p>
-          <p>{selection === "choose" && <ApodChoose />}</p>
-          <p>{selection === "gimme" && <ApodGimme5 />}</p>
-          <p>{selection === "range" && <ApodRange />}</p>
-        </div> */}
+        </div >
+  <div className="content_flexbox">     
         <Outlet />
       </div>
-
+ </div >
     </>
   );
 }
