4393839bc084d7f1058f66693bdcbdfeb05e9eff
Sunflair0 <62845098+Sunflair0@users.noreply.github.com>
2022-08-10 18:38:08 -0500

    apod buttons good, vh ok

--- src/pages/NASA/APOD/APOD.js
+++ src/pages/NASA/APOD/APOD.js
@@ -1,49 +1,15 @@
-import React, { useState, } from 'react';
-import { NavLink, Outlet } from 'react-router-dom';
+import AButtons from './AButtons'
+import { Outlet } from 'react-router-dom';
 
 export default function Apod() {
-  const [selection, setSelection] = useState("")
-
   return (
     <>
-      <div className='apodPage'>        
-        <div className="apodButtons">
-          <NavLink to="apodtoday">
-            <div className='apodButton today'
-              style={{
-                backgroundImage: "url(../../assets/today.png)"
-              }}
-              onClick={() => setSelection(selection)} />
-          </NavLink>
-
-          <NavLink to="apodchoose">
-            <div className='apodButton choose'
-              style={{
-                backgroundImage: "url(../../assets/choose.png)"
-              }}
-              onClick={() => setSelection(selection)} />
-          </NavLink>
-
-          <NavLink to="apodgimme5">
-            <div className='apodButton gimme'
-              style={{
-                backgroundImage: "url(../../assets/gimme.png)"
-              }}
-              onClick={() => setSelection(selection)} />
-          </NavLink>
-
-          <NavLink to="apodrange">
-            <div className='apodButton range'
-              style={{
-                backgroundImage: "url(../../assets/range.png)"
-              }}
-              onClick={() => setSelection(selection)} />
-          </NavLink>
-        </div >
-  <div className="content_flexbox2">     
+<div className='apodPage'>
+      <AButtons />
+      <div className="content_flexbox2">
         <Outlet />
       </div>
- </div >
+</div>
     </>
   );
 }
