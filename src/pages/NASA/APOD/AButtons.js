11a9db5cca2eb0c967cb15efe29c141620928ebc
Sunflair0 <62845098+Sunflair0@users.noreply.github.com>
2022-08-10 19:46:33 -0500

    vh of non apod pages

--- src/pages/NASA/APOD/AButtons.js
+++ src/pages/NASA/APOD/AButtons.js
@@ -1,42 +1,42 @@
 import React, { useState, } from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Apod() {
   const [selection, setSelection] = useState("")
 
   return ( 
         <div className="apodButtons">
-          <NavLink to="apodtoday">
+          <Link to="apodtoday">
             <div className='apodButton today'
               style={{
                 backgroundImage: "url(../../assets/today.png)"
               }}
               onClick={() => setSelection(selection)} />
-          </NavLink>
+          </Link>
 
-          <NavLink to="apodchoose">
+          <Link to="apodchoose">
             <div className='apodButton choose'
               style={{
                 backgroundImage: "url(../../assets/choose.png)"
               }}
               onClick={() => setSelection(selection)} />
-          </NavLink>
+          </Link>
 
-          <NavLink to="apodgimme5">
+          <Link to="apodgimme5">
             <div className='apodButton gimme'
               style={{
                 backgroundImage: "url(../../assets/gimme.png)"
               }}
               onClick={() => setSelection(selection)} />
-          </NavLink>
+          </Link>
 
-          <NavLink to="apodrange">
+          <Link to="apodrange">
             <div className='apodButton range'
               style={{
                 backgroundImage: "url(../../assets/range.png)"
               }}
               onClick={() => setSelection(selection)} />
-          </NavLink>
+          </Link>
         </div >
        );
 }
