f0d3f9deeaa46ddca6fd5f759a5587f375151273
Sunflair0 <62845098+Sunflair0@users.noreply.github.com>
2022-08-25 10:59:35 -0500

    blackhole in Junk

--- src/components/Junk.js
+++ src/components/Junk.js
@@ -3,17 +3,26 @@ import React from 'react'
 const Junk = () => {
 	return (
 		<>
-			<div className="content_flexbox">
-				<div className="myBackground">
-					<div className="circle3"></div>
-					<div className="boxFade">
-						<div className="fadeBox">
-							<div className="iris"></div>
-							<div className="bubble"></div>
-							<div className="marsa"></div>
-						</div>
+			<div className="black_box">
+				<div className="my_page_background1">
+					<div className='accretion_disc' />
+				</div>
+
+				<div className="black_hole" >
+					<div className="my_page_background2">
+						<div className='accretion_disc' />
 					</div>
 				</div>
+				{/* <div className="content_flexbox">
+								<div className="circle32"></div>
+								<div className="boxFade2">
+									<div className="fadeBox2">
+										<div className="iris2"></div>
+										<div className="bubble2"></div>
+										<div className="marsa2"></div>
+									</div>
+								</div>
+							</div> */}
 			</div>
 		</>
 	)
