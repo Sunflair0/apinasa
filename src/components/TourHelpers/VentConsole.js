5de866821913b3cc888133cd9c2f040e2688ccac
Sunflair0 <62845098+Sunflair0@users.noreply.github.com>
2022-11-19 18:06:44 -0600

    npm i

--- src/components/TourHelpers/VentConsole.js
+++ src/components/TourHelpers/VentConsole.js
@@ -36,33 +36,3 @@ const showSidebar2 = () => setSidebar2(!sidebar2)
     </>
   )
 }
-
-
-
-// export default function MenuPop() {
-//   const [isPop, setIsPop] = useState(false);
-// return (
-// <>
-// <ul className="nav-menu-items">
-// return( 
-// MenuPop.map((item, index) => {
-
-// <li className="wrapper"><button onClick={() => setIsPop(true)}>
-// <div key={index}>
-// <Link to={item.path}>
-//  {item.img}
-// <span>{item.tour}</span> 
-
-// <BigCube open={isPop} onClose ={()=> setIsPop(false)}></BigCube>
-// <div className="more">{BigCube()}</div>
-
-// </Link>
-// </div>
-// </button>
-// </li>
-// }))
-// </ul>
-
-// </>)}
-
-
